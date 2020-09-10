2c6fa563fa081d953a7b4446d144d9b317535870
”ayako” <poohpooh0616@yahoo.co.jp>
2020-09-10 06:40:45 +0000

    課題の提出No.30「PHP/Laravel Herokuへのデプロイ」

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //Herokuへのデプロイ
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
